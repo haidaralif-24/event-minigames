b693cbc8c13dc9b9b301c8134d9c8fba1f933333
Haidar Ali Fawwaz Nasirodin <haidar.nasirodin@gdajogja.sch.id>
2026-08-12 21:43:51 +0700

    Move host controls into dedicated sidebar

--- src/pages/Host.jsx
+++ src/pages/Host.jsx
@@ -3,12 +3,19 @@ import HostControls from '../components/HostControls.jsx';
 
 export default function HostPage() {
   return (
-    <div className="relative min-h-screen w-full overflow-hidden">
-      <Board />
-      {/* Floating control panel */}
-      <div className="absolute right-4 top-4 z-20 w-80">
-        <HostControls />
-      </div>
+    <div className="flex h-screen w-full overflow-hidden bg-[#152033]">
+      <main className="min-w-0 flex-1">
+        <Board />
+      </main>
+      <aside className="z-30 flex h-full w-[340px] shrink-0 flex-col border-l-4 border-[#18233f] bg-[#f7f2df] shadow-2xl">
+        <div className="border-b-2 border-[#18233f]/15 px-5 py-4">
+          <p className="text-xs font-black uppercase tracking-[0.2em] text-[#6d7890]">Party Board</p>
+          <h1 className="font-display text-2xl text-[#18233f]">Game Control</h1>
+        </div>
+        <div className="min-h-0 flex-1 overflow-y-auto p-4">
+          <HostControls />
+        </div>
+      </aside>
     </div>
   );
 }
